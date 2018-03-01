5d48a2c6abf41e3b6a036e087aebd381d078f77a
Diana Adiema <adiemadiana2@gmail.com>
2018-03-01 17:08:43 +0300

    Phonebook app with TDD

---
+++ phonebook.py
@@ -0,0 +1,60 @@
+import uuid
+
+
+class PhoneBook:
+
+    def __init__(self):
+       self.phonebook_dict = {}
+
+    def add_contact(self, name, email, number):
+    	contact_uuid = uuid.uuid4()
+    	self.phonebook_dict[contact_uuid] = {
+    	'name':name, 
+    	'email':email,
+    	'tel':number,
+    	'uuid': contact_uuid
+    	}
+
+    	return self.phonebook_dict
+
+    def search(self,field, search_param):
+	    for k, v in self.phonebook_dict.items():
+	        if v.get(field) == search_param:
+	            return v
+	    return None
+
+
+       
+ 
+    def update_contact(self,uuid,fields): 
+    	"""
+    	Fields should be a dictionary containing either email,name,tel. to search find uuid before deleting
+
+
+    	"""
+    	contact = self.phonebook_dict.get(uuid,None) 
+
+    	if contact is None:
+    		return None
+    	contact.update(fields) #searches using uuid then updates rest of field attached to uuid.
+
+
+    def delete_contact(self, uuid):
+    	contact = self.phonebook_dict.get(uuid,None) #Search for contact to be delete, if not there return none
+    	if contact is None:
+    		return None
+    	self.phonebook_dict.pop(uuid) #delete the uuid(key) which deletes the fields aligned to it
+    	return self.phonebook_dict
+
+    def view_contact(self):
+
+        return self.phonebook_dict
+
+
+
+    	
+        
+
+        
+
+
---
+++ test_phonebook.py
@@ -0,0 +1,38 @@
+import unittest
+from phonebook import PhoneBook
+
+
+class PhoneBookTestCase(unittest.TestCase):
+    def setUp(self):
+        self.phonebook = PhoneBook()
+        self.search = self.phonebook.search
+        self.add_contact = self.phonebook.add_contact
+        self.delete_contact = self.phonebook.delete_contact
+        self.view_contact = self.phonebook.view_contact
+
+    def test_search(self):
+        self.add_contact('jane','diana@email.com','947894704')
+        self.add_contact('diana','diana@email.com','9353835')
+        actual = self.search('name','diana')
+        print(actual)
+        self.assertIsNotNone(actual)
+
+        self.assertEquals(actual['name'],'diana')
+
+    def test_add(self):
+        actual = self.add_contact('diana','diana@email.com','9353835')
+        results = self.search('name', 'diana')
+        self.assertIsNotNone(actual)
+        self.assertIsNotNone(results)
+        self.assertEquals(results['tel'],'9353835')
+
+    def test_delete(self):
+        self.add_contact('diana','diana@email.com','9353835',uuid) #we need this for test
+        results = self.search('name', 'diana') #search for it
+        self.assertIsNotNone(results) #confirm it exists
+        self.delete_contact(results[uuid]) #Deletes it regarding the key field,uuid
+        results2 = self.search(results[uuid]) #searches to comfirm deletion
+        self.assertIsNone(results2) #it does noe exist. Deletion successfull
+
+    # def test_view_contact():
+    #     pass
